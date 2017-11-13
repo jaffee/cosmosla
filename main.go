af974ad747e0396b3d79ff3628ac860b32dfcd0a
Matt Jaffee <jaffee@pilosa.com>
2017-11-13 11:08:43 -0600

    updates

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/Azure/go-autorest/autorest/utils"
@@ -32,15 +33,17 @@ type Person struct {
 }
 
 type Main struct {
-	Num    int
-	Insert bool
-	Query  bool
-	Seed   int64
+	Num         int
+	Insert      bool
+	Concurrency int
+	Query       bool
+	Seed        int64
 }
 
 func main() {
 	m := Main{}
 	flag.IntVar(&m.Num, "num", 10000, "number of docs to insert")
+	flag.IntVar(&m.Concurrency, "concurrency", 1, "number of goroutines doing insertion")
 	flag.BoolVar(&m.Insert, "insert", false, "do insertions")
 	flag.BoolVar(&m.Query, "query", false, "do queries")
 	flag.Int64Var(&m.Seed, "seed", 1, "seed for rng")
@@ -52,6 +55,7 @@ func main() {
 }
 
 func (m *Main) Run() error {
+	rand.Seed(m.Seed)
 	// DialInfo holds options for establishing a session with a MongoDB cluster.
 	dialInfo := &mgo.DialInfo{
 		Addrs:    []string{fmt.Sprintf("%s.documents.azure.com:10255", database)}, // Get HOST + PORT
@@ -77,16 +81,39 @@ func (m *Main) Run() error {
 	// If the safe parameter is nil, the session is put in unsafe mode, and writes become fire-and-forget,
 	// without error checking. The unsafe mode is faster since operations won't hold on waiting for a confirmation.
 	// http://godoc.org/labix.org/v2/mgo#Session.SetMode.
+	session.SetMode(mgo.Eventual, false)
 	session.SetSafe(&mgo.Safe{})
 
 	// get collection
 	collection := session.DB(database).C("people")
 
 	if m.Insert {
-		err := m.Write(collection)
-		if err != nil {
-			return errors.Wrap(err, "inserting")
+		writes := make(chan struct{}, m.Concurrency)
+		cancel := make(chan error, 1)
+		wg := sync.WaitGroup{}
+		start := time.Now()
+		for i := 0; i < m.Concurrency; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err := m.Write(writes, collection)
+				if err != nil {
+					select {
+					case cancel <- err:
+					}
+				}
+			}()
 		}
+		for i := 0; i < m.Num; i++ {
+			select {
+			case err := <-cancel:
+				return errors.Wrap(err, "writing")
+			case writes <- struct{}{}:
+			}
+		}
+		close(writes)
+		wg.Wait()
+		log.Printf("writing %v docs took %v\n", m.Num, time.Since(start))
 	}
 
 	if m.Query {
@@ -98,11 +125,10 @@ func (m *Main) Run() error {
 	return nil
 }
 
-func (m *Main) Write(collection *mgo.Collection) error {
-	g := NewGenerator(m.Seed)
+func (m *Main) Write(writes chan struct{}, collection *mgo.Collection) error {
 	// insert documents into collection
-	for i := 0; i < m.Num; i++ {
-		err := collection.Insert(g.Person())
+	for range writes {
+		err := collection.Insert(GenPerson())
 		if err != nil {
 			return errors.Wrap(err, "inserting person")
 		}
@@ -110,33 +136,22 @@ func (m *Main) Write(collection *mgo.Collection) error {
 	return nil
 }
 
-type Generator struct {
-	r *rand.Rand
-}
-
-func NewGenerator(seed int64) Generator {
-	src := rand.NewSource(seed)
-	return Generator{
-		r: rand.New(src),
-	}
-}
-
 var letters = "abcdefghijklmnopqrstuvwxyz12345678"
 
-func (t Generator) Tile() string {
-	length := t.r.Intn(4) + 2
+func Tile() string {
+	length := rand.Intn(4) + 2
 	ret := make([]byte, length)
 	for i := 0; i < length; i++ {
-		ret[i] = letters[t.r.Intn(len(letters))]
+		ret[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(ret)
 }
 
-func (t Generator) Person() *Person {
+func GenPerson() *Person {
 	tiles := make(map[string]bool)
-	num := t.r.Intn(930) + 70
+	num := rand.Intn(930) + 70
 	for i := 0; i < num; i++ {
-		tiles[t.Tile()] = true
+		tiles[Tile()] = true
 	}
 	return &Person{
 		Tiles: tiles,
--- query.go
+++ query.go
@@ -10,12 +10,19 @@ import (
 )
 
 func (m *Main) Read(collection *mgo.Collection) error {
+	n, err := collection.Find(nil).Count()
+	if err != nil {
+		return errors.Wrap(err, "counting all records")
+	}
+	fmt.Printf("total records: %v\n", n)
+
 	start := time.Now()
-	n, err := collection.Find(bson.M{"tiles.os": true, "tiles.du": true}).Count()
+	res := Person{}
+	err = collection.Find(bson.M{"tiles.os": true, "tiles.du": true}).One(&res)
 	duration := time.Since(start)
 	if err != nil {
 		return errors.Wrap(err, "finding record")
 	}
-	fmt.Printf("%v results in %v\n", n, duration)
+	fmt.Printf("%v results in %v\n", res, duration)
 	return nil
 }
