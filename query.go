6e8f40004d086bdf5873a8473bf9f102c1d02b9d
Matt Jaffee <jaffee@pilosa.com>
2017-11-13 11:38:56 -0600

    more queries

--- query.go
+++ query.go
@@ -10,19 +10,40 @@ import (
 )
 
 func (m *Main) Read(collection *mgo.Collection) error {
+	start := time.Now()
 	n, err := collection.Find(nil).Count()
 	if err != nil {
 		return errors.Wrap(err, "counting all records")
 	}
-	fmt.Printf("total records: %v\n", n)
+	fmt.Printf("%v total records: %v\n", time.Since(start), n)
 
-	start := time.Now()
 	res := Person{}
-	err = collection.Find(bson.M{"tiles.os": true, "tiles.du": true}).One(&res)
-	duration := time.Since(start)
+	start = time.Now()
+	err = collection.Find(nil).One(&res)
 	if err != nil {
-		return errors.Wrap(err, "finding record")
+		return errors.Wrap(err, "finding first record")
 	}
-	fmt.Printf("%v results in %v\n", res, duration)
+	fmt.Printf("%v first result:\n%v\n", time.Since(start), res)
+
+	start = time.Now()
+	res = Person{}
+	err = collection.Find(bson.M{"tiles.os": true, "tiles.du": true}).One(&res)
+	if err != nil {
+		return errors.Wrap(err, "finding first segment record")
+	}
+	fmt.Printf("%v first segment record:\n%v\n", time.Since(start), res)
+
+	res = Person{}
+	start = time.Now()
+	// idbytes, err := hex.DecodeString("5a09a414f21bc91a5c7e7669")
+	// if err != nil {
+	// 	return errors.Wrap(err, "decoding hex")
+	// }
+	err = collection.Find(bson.M{"_id": bson.ObjectIdHex("5a09a414f21bc91a5c7e7669")}).One(&res)
+	if err != nil {
+		return errors.Wrap(err, "finding single record")
+	}
+	fmt.Printf("%v single record %v\n", time.Since(start), res)
+
 	return nil
 }
